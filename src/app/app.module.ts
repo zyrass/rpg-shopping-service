14031cd7ed77e8da158d33371d0820bcd0aee9b2
Alain Guillon <zyrass@outlook.fr>
2024-03-01 16:02:11 +0100

    Refactorisation avec l'ajout de deux composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './components/title/title.component';
+import { ShoppingContainerComponent } from './components/shopping-container/shopping-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent,
+    ShoppingContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shopping-container/shopping-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-container',
+  templateUrl: './shopping-container.component.html'
+})
+export class ShoppingContainerComponent {
+
+}
